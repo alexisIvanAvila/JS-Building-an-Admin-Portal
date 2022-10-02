bc7b15fde05069e2cbad3881f73d97ee9060c5f9
alexis avila <evan.avila1@gmail.com>
2022-10-01 20:10:43 -0700

    completed activity

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
+ 
+let addbutton = document.querySelector('#add-book')
 
-// Your Code Here
+
+
+async function getBooks () {
+    let newId = 0;
+    let response = await fetch('http://localhost:3001/listBooks')
+    
+    let data = await response.json();
+    
+    data.forEach(book => {
+        if (book.id > new Id) {
+            newId = book.id
+        }
+    })
+
+   return newId + 1;
+}
+  
+addbutton.addEventListener('click', async () => {
+  let id = await assignId(); 
+})
+
+addbutton.addEventListener('click', () => {
+let dataToSend = {
+    id: id,
+    title:document.querySelector('#add-title').value,
+    year:document.querySelector('#add-year').value,
+    description:document.querySelector('#add-description').value,
+    quantity:document.querySelector('#add-quantity').value,
+    imageURL:document.querySelector('#add-imageURL').value,
+  }
+  await fetch('http://localhost:3001/listBooks', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataToSend)
+  })
+
+  document.querySelector('#add-title').value = '',
+  document.querySelector('#add-year').value = '',
+  document.querySelector('#add-description').value = '',
+  document.querySelector('#add-quantity').value = '',
+  document.querySelector('#add-imageURL').value = '',
+
+}) 
